ba4dff0632fd5d5a773dccb74d3ee373c4d70cb3
Hala Alkhellow <halakhellow1996@gmail.com>
2022-04-12 00:21:07 +0300

    add selectingDrivers array which contains the returnFirstTwoDrivers and returnLastTwoDrivers functions

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
 const returnFirstTwoDrivers = (drivers) => [drivers[0],drivers[1]]
 
 const returnLastTwoDrivers = (drivers) => [drivers[drivers.length-2],drivers[drivers.length-1]]
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
